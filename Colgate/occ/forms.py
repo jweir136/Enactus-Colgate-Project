0738e11db4e61c0778493cb8215ebb6058b86217
jweir136 <jweir136@yahoo.com>
2022-05-05 09:32:01 -0400

    add proper passwords to signup and login forms

--- Colgate/occ/forms.py
+++ Colgate/occ/forms.py
@@ -5,11 +5,11 @@ class SignUpForm(forms.Form):
     last_name = forms.CharField(max_length=100)
     email = forms.EmailField()
     username = forms.CharField(max_length=100)
-    password = forms.CharField(max_length=100)#forms.PasswordInput()
+    password = forms.CharField(widget=forms.PasswordInput())
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=100)
-    password = forms.CharField(max_length=100)#forms.PasswordInput()
+    password = forms.CharField(widget=forms.PasswordInput())
 
 class LearningModuleSubmissionForm(forms.Form):
     forminput = forms.CharField(max_length=100)
